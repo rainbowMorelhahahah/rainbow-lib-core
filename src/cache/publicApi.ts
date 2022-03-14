19bb911f66ab086f41b6790024c1c089c6a87050
rex <rainbowmorel@163.com>
2022-03-14 11:20:37 +0800

    add cookie localStoreage func

--- src/cache/publicApi.ts
+++ src/cache/publicApi.ts
@@ -1,3 +1,4 @@
 export { ICache } from './interface/cache-face'
 export { default as CookieFactory } from './factory/cookie-factory';
-export { default as LocalStorageFactory } from './factory/local-storeage-factory'
+export { default as LocalStorageFactory } from './factory/local-storeage-factory';
+export { cookie, localStoreage } from './func'
