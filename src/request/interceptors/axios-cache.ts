c6e97b29ad1f4da030b3a5c754cda8f4ab4c7f0d
rex <rainbowmorel@163.com>
2022-03-17 14:46:18 +0800

    请求缓存最大保存  20 条

--- src/request/interceptors/axios-cache.ts
+++ src/request/interceptors/axios-cache.ts
@@ -8,6 +8,8 @@ export default class AxiosCacheInterceptor implements IHttpInterceptors {
 
   private cache: Map<string | undefined, any> = new Map<string, any>();
 
+  private capacity: number = 20;
+
   handleHttpRequst(config: AxiosRequestConfig): AxiosRequestConfig {
     //只对GET请求进行缓存
     if (config.method !== 'GET'.toLocaleLowerCase()) { return config }
@@ -17,7 +19,7 @@ export default class AxiosCacheInterceptor implements IHttpInterceptors {
     config.cancelToken = source.token;
     const cahceKey = `${config.url}${JSON.stringify(config.params)}`;
     let data = this.cache.get(MD5(cahceKey).toString());
-  
+
     const CACHETIMEOUT = AppConfig()?.request?.cacheTimeOut || 60000;
     if (data && GetTimeNowUnxi() - data.expire < Number(CACHETIMEOUT)) {
       source.cancel(data);
@@ -33,6 +35,10 @@ export default class AxiosCacheInterceptor implements IHttpInterceptors {
     if (response.config !== undefined) {
       //如果是缓存不做处理
       const cahceKey = `${response.config.url}${JSON.stringify(response.config.params)}`;
+      if (this.cache.size >= this.capacity) {
+        // 缓存超过最大值，则移除最近没有使用的
+        this.cache.delete(this.cache.keys().next().value)
+      }
       this.cache.set(MD5(cahceKey).toString(), data);
     }
     return response;
